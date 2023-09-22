48c01287b5673374697c4447a333c27cfc54372c
Lietrix <Joshdot@gmail.com>
2023-09-22 15:26:32 -0700

    Seperated functionality

--- main.cpp
+++ main.cpp
@@ -5,68 +5,85 @@
 #include <cmath>
 using namespace std;
 
+bool readFile(const string &fileName, vector<int> &ccData, vector<int> &yearData) {
+    ifstream inFile(fileName);  
+    if (!inFile) {
+        cerr << "Failed to open " << fileName << endl;
+        return false;
+    }
+
+    int cc, kya;
+    while (inFile >> cc >> kya) {
+        ccData.push_back(cc);
+        yearData.push_back(kya);
+    }
+    inFile.close();
+    return true;
+}
+
+void findLargestCC(const vector<int> &ccData, const vector<int> &yearData, int &largestCC, int &correspondingYear) {
+    largestCC = ccData[0];
+    int index = 0;
+    for (int i = 1; i < ccData.size(); i ++) {
+        if (ccData[i] > largestCC) {
+            largestCC = ccData[i];
+            index = i;
+        }
+    }
+    correspondingYear = yearData[index];
+}
+
+double calculateMean(const vector<int> &ccData) {
+    double totalCC = 0;
+    for (int i = 0; i < ccData.size(); i++) {
+        totalCC += ccData[i];
+    }
+    return totalCC / static_cast<double>(ccData.size());
+}
+
+double calculateStdDev(const vector<int> &ccData, double mean) {
+    double variance = 0.0;
+    for (int i = 0; i < ccData.size(); i++) {
+        variance += (ccData[i] - mean) * (ccData[i] - mean);
+    }
+    variance /= ccData.size();
+    return sqrt(variance);
+}
+
+double calculateCV(double stdDeviation, double mean) {
+    return (stdDeviation / mean) * 100.0;
+}
+
 int main() {
     string fileName;
     cout << "Enter the name of the file to read: ";
     cin >> fileName;
 
-    ifstream inFile(fileName);  // Open the file for reading
-
-    if (!inFile) {  // Check if the file was opened successfully
-        cerr << "Failed to open " << fileName << endl;
-        return 1;  // Exit with an error code
-    }
-    
     vector<int> ccData;
     vector<int> yearData;
 
-    int cc, kya;
-    while (inFile >> cc >> kya) {  
-        ccData.push_back(cc);
-        yearData.push_back(kya);
+    if (!readFile(fileName, ccData, yearData)) {
+        return 1;
     }
 
-
     if(!ccData.empty()){
-        // Largest CC 
-        int largest_cc = ccData[0];
-        int index = 0;
-        for (int i = 1; i < ccData.size(); i ++) {
-            if (ccData[i] > largest_cc) {
-                largest_cc = ccData[i];
-                index = i;
-            }
-        }
-        cout << "Largest CC: " << largest_cc << endl;
-        cout << "Kya: " << yearData[index] << endl;
+        int largestCC, correspondingYear;
+        findLargestCC(ccData, yearData, largestCC, correspondingYear);
+        cout << "Largest CC: " << largestCC << endl;
+        cout << "Kya: " << correspondingYear << endl;
 
-        // Mean CC
-        double meanCC;
-        double totalCC;
-        for (int i = 0; i < ccData.size(); i++) {
-            totalCC += ccData[i];
-        }
-        meanCC = totalCC /  static_cast<double>(ccData.size());
+        double meanCC = calculateMean(ccData);
         cout << "Mean CC: " << meanCC << endl;
 
-        //standard deviation
-        double variance = 0.0;
-        for (int i = 0; i < ccData.size(); i++) {
-            variance += (ccData[i] - meanCC) * (ccData[i] - meanCC);
-        }
-        variance /= ccData.size();
-        double stdDeviation = sqrt(variance);
+        double stdDeviation = calculateStdDev(ccData, meanCC);
         cout << "Standard Deviation CC: " << stdDeviation << endl;
 
-        //CV
-        double CV = (stdDeviation / meanCC) * 100.0;
+        double CV = calculateCV(stdDeviation, meanCC);
         cout << "Coefficient of Variation (CV): " << CV << "%" << endl;
     }
     else {
         cout << "No data found." << endl;
     }
-
-    inFile.close();  // Close the file
 
     return 0;  
 }
